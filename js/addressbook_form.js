2193dd84402ce72a5387f8e02147ea96857215d6
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-18 11:18:42 +0530

    on submit add to local storage

--- js/addressbook_form.js
+++ js/addressbook_form.js
@@ -17,23 +17,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-    // const addressElement = document.querySelector('#address');
-    // const addressError = document.querySelector('#address-error');
-    // addressElement.addEventListener('input', function () {
-    //     let address = document.querySelector('#address').value;
-    //     if (addressElement.value.length == 0) {
-    //         addressError.textContent = '';
-    //         return;
-    //     }
-    //     try {
-    //         (new Contact()).address = address;
-    //         addressError.textContent = '';
-    //         return;
-    //     } catch (e) {
-    //         addressError.textContent = e;
-    //     }
-    // });
-
 
     const phoneElement = document.querySelector('#phone');
     const phoneError = document.querySelector('.phone-error');
@@ -47,8 +30,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
             phoneError.textContent = e;
         }
     });
-
-
 })
 
 const save = (event)=>{
@@ -56,6 +37,7 @@ const save = (event)=>{
 //   event.stopPropagation();
   try{
     setContactObject();
+    createAddressBook();
   }catch(e){
     console.log(e);
     return;
@@ -63,7 +45,6 @@ const save = (event)=>{
 }
 
 const setContactObject = () => {
-    
     let names = getInputValueById('#name').split(" ");
     contactObject._firstName = names[0];
     contactObject._lastName = names[1];
@@ -73,7 +54,22 @@ const setContactObject = () => {
     contactObject._zip = getInputValueById('#zip');
     contactObject._phone = getInputValueById('#phone');
     contactObject._email = getInputValueById('#email');
-    alert(JSON.stringify(contactObject))
+    // alert(JSON.stringify(contactObject))
+  }
+
+  const createAddressBook = () =>{
+      let addressbook = new Contact();
+      let names = getInputValueById('#name').split(" ");
+    addressbook._firstName = names[0];
+    addressbook._lastName = names[1];
+    addressbook._address = getInputValueById('#address');
+    addressbook._city = getInputValueById('#city');
+    addressbook._state = getInputValueById('#state');
+    addressbook._zip = getInputValueById('#zip');
+    addressbook._phone = getInputValueById('#phone');
+    addressbook._email = getInputValueById('#email');
+    alert(addressbook)
+
   }
 
   const getInputValueById = (id) => {
