020b5bc89b9889f5c3720da4e9683e837e9b5a68
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-18 11:02:19 +0530

    added as json in local storage

--- js/addressbook_form.js
+++ js/addressbook_form.js
@@ -17,6 +17,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
+    // const addressElement = document.querySelector('#address');
+    // const addressError = document.querySelector('#address-error');
+    // addressElement.addEventListener('input', function () {
+    //     let address = document.querySelector('#address').value;
+    //     if (addressElement.value.length == 0) {
+    //         addressError.textContent = '';
+    //         return;
+    //     }
+    //     try {
+    //         (new Contact()).address = address;
+    //         addressError.textContent = '';
+    //         return;
+    //     } catch (e) {
+    //         addressError.textContent = e;
+    //     }
+    // });
+
 
     const phoneElement = document.querySelector('#phone');
     const phoneError = document.querySelector('.phone-error');
@@ -35,7 +52,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 })
 
 const save = (event)=>{
-
+//     event.preventDefault();
+//   event.stopPropagation();
   try{
     setContactObject();
   }catch(e){
@@ -45,7 +63,7 @@ const save = (event)=>{
 }
 
 const setContactObject = () => {
-    let contactObject = new Contact()
+    
     let names = getInputValueById('#name').split(" ");
     contactObject._firstName = names[0];
     contactObject._lastName = names[1];
@@ -55,7 +73,7 @@ const setContactObject = () => {
     contactObject._zip = getInputValueById('#zip');
     contactObject._phone = getInputValueById('#phone');
     contactObject._email = getInputValueById('#email');
-    alert(contactObject.toString())
+    alert(JSON.stringify(contactObject))
   }
 
   const getInputValueById = (id) => {
